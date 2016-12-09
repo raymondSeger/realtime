5ad3de8979a97a25341f6532ee019d521708acb2
Raymond Seger <raymondseger@live.com>
2016-12-09 17:07:08 +0700

    use cors example

--- mainEntryPoint.js
+++ mainEntryPoint.js
@@ -1,23 +1,29 @@
 var express         = require('express');
 var app             = express();
-var cookieParser    = require('cookie-parser');
+var cors            = require('cors');
 
-app.use( cookieParser() );
+
+app.use( cors() );
 app.use( '/public', express.static( __dirname + '/public' ) );
+
+
+
 
 // create cookie in middleware
 app.use('/', function (req, res, next) {
-    var cookie = req.cookies.cookieName;
-    if (cookie === undefined) {
-        res.cookie('cookieName', 'raymondSeger', { maxAge: 900000, httpOnly: false });
-        console.log('cookie created successfully');
-    }
     next();
 });
 
+
 app.get('/', function (req, res) {
-    res.send( req.cookies );
+    res.send( 'a' );
 });
+
+
+
+
+
+
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
