798a942539c9db47192360100c205e804dc8e61c
Raymond Seger <raymondseger@live.com>
2016-12-09 17:00:16 +0700

    example of creating cookie, the first time it will create the cookie and send it to user, BUT the server cannot detect the cookie value so remember to use variable value inside that server (which is the value you set for the cookie that will be sent to the user)

--- mainEntryPoint.js
+++ mainEntryPoint.js
@@ -1,12 +1,22 @@
-var express     = require('express');
-var app         = express();
+var express         = require('express');
+var app             = express();
+var cookieParser    = require('cookie-parser');
 
-
-
+app.use( cookieParser() );
 app.use( '/public', express.static( __dirname + '/public' ) );
 
+// create cookie in middleware
+app.use('/', function (req, res, next) {
+    var cookie = req.cookies.cookieName;
+    if (cookie === undefined) {
+        res.cookie('cookieName', 'raymondSeger', { maxAge: 900000, httpOnly: false });
+        console.log('cookie created successfully');
+    }
+    next();
+});
+
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.send( req.cookies );
 });
 
 app.listen(3000, function () {
