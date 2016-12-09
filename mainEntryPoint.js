1c6a44038d8077af16262395f4bb59e0cc190fd8
Raymond Seger <raymondseger@live.com>
2016-12-09 16:31:49 +0700

    example of colors nodeJS. Make the console log have colors. Only work with Command Prompt

--- mainEntryPoint.js
+++ mainEntryPoint.js
@@ -1,5 +1,15 @@
 var express     = require('express');
 var app         = express();
+var colors      = require('colors');
+
+console.log('hello'.green); // outputs green text
+console.log('i like cake and pies'.underline.red); // outputs red underlined text
+console.log('inverse the color'.inverse); // inverses the color
+console.log('OMG Rainbows!'.rainbow); // rainbow
+console.log('Run the trap'.trap); // Drops the bass
+
+var name = 'Marak';
+console.log(colors.green('Hello %s'), name);
 
 app.use( '/public', express.static( __dirname + '/public' ) );
 
