a5a8ec669b86ff70b600e95e0b6d198f83a37e0f
Raymond Seger <raymondseger@live.com>
2016-12-11 10:09:28 +0700

    moment npm for dates, time

--- mainEntryPoint.js
+++ mainEntryPoint.js
@@ -1,15 +1,9 @@
 var express     = require('express');
 var app         = express();
-var helmet      = require('helmet');
-
+var moment      = require('moment');
 
 // static for files
 app.use( '/public', express.static( __dirname + '/public' ) );
-
-
-app.use(helmet.frameguard());
-app.use(helmet.hidePoweredBy());
-app.use(helmet.dnsPrefetchControl());
 
 /*
  * Middlewares
@@ -23,6 +17,11 @@ app.use('/', function (req, res, next) {
  * Routes
  */
 app.get('/', function (req, res) {
+    console.log ( moment().format('MMMM Do YYYY, h:mm:ss a') ); // December 11th 2016, 10:08:46 am
+    console.log ( moment().format('dddd') );                     // Sunday
+    console.log ( moment().format("MMM Do YY") );               // Dec 11th 16
+    console.log ( moment().format('YYYY [escaped] YYYY') );      // 2016 escaped 2016
+    console.log ( moment().format() );
     res.send( 'a' );
 });
 
