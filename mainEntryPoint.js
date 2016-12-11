dd0afdb359a30d8f9601c6dfdb2aa8e1f3cc8061
Raymond Seger <raymondseger@live.com>
2016-12-11 11:23:39 +0700

    validator npm example to check if it's email or not (just one basic example)

--- mainEntryPoint.js
+++ mainEntryPoint.js
@@ -1,6 +1,6 @@
 var express     = require('express');
 var app         = express();
-var request = require('request');
+var validator   = require('validator');
 
 // static for files
 app.use( '/public', express.static( __dirname + '/public' ) );
@@ -17,13 +17,10 @@ app.use('/', function (req, res, next) {
  * Routes
  */
 app.get('/', function (req, res) {
-    request('http://www.google.com', function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            console.log(error);
-            console.log(response);
-            res.send( body );
-        }
-    });
+
+    console.log( validator.isEmail('foo@bar.com') );
+    res.send( 'a' );
+
 });
 
 app.listen(3000, function () {
